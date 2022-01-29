899b1fad876669d47c670893a29db95721a3133c
zob_c <zob_c@DESKTOP-AG9ADM7>
2022-01-29 09:12:23 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
